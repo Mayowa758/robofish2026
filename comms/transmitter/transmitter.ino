63f55cb324d838e0a07354bfab739603a27c65fa
Josh <jcosmosmakes@gmail.com>
2026-06-11 20:27:01 +1000

    more stuff

--- comms/transmitter/transmitter.ino
+++ comms/transmitter/transmitter.ino
@@ -1,30 +1,68 @@
+/*
+  TRANSMITTER
+*/
+
 #include <SoftwareSerial.h>
+#define DE_RE 9
+#define RS485_RX 10
+#define RS485_TX 11
 
-#define ENABLE_PIN 8
-#define RX_PIN 10
-#define TX_PIN 11
+#define TRANSMIT_MODE HIGH
+#define RECIEVE_MODE LOW
 
-SoftwareSerial mySerial(RX_PIN, TX_PIN);
+
+SoftwareSerial rs485(RS485_RX, RS485_TX);
+
+
+typedef enum Instruction_ {
+  CHANGE_SPEED, 
+  TURN_LEFT_FLIPPER,
+  TURN_RIGHT_FLIPPER,
+  TURN_TOP_FLIPPER,
+  CALIBRATE_ANGLES
+} Instruction;
+
 void setup() {
-
-  Serial.begin(115200);
-  mySerial.begin(9600);
-
-  
-  pinMode(ENABLE_PIN, OUTPUT);
-
-  digitalWrite(ENABLE_PIN, HIGH);
+  pinMode(DE_RE, OUTPUT);
+  digitalWrite(DE_RE, TRANSMIT_MODE);
+  Serial.begin(9600);
+  rs485.begin(4800);
+}
 
 
+// Encode Instruction
+// INSTRUCTIONS TO BE SENT
+// 1: CHANGE_SPEED {option}
+// 2: TURN_LEFT_FLIPPER {Angle} 
+// 3: TURN_RIGHT_FLIPPER {Angle}
+// 4: TURN_TOP_FLIPPER {Angle}
+// 5: CALIBRATE_ANGLES
+
+// EACH STEP IS 180/32 approx 6 degrees
+
+
+// 000       00000
+// ^ inst   ^ paramater
+
+// E.G TURN_LEFT 30 (2 5)
+// Instruction sent: 2 5 -> 010 00101
+
+uint8_t encode_message(Instruction inst, uint8_t paramater) {
+  return 0;
+}
+
+// Transmit message
+void transmit(String message) {
+  digitalWrite(DE_RE, TRANSMIT_MODE);
+  rs485.println(message);
+  delay(10);
+  digitalWrite(DE_RE, RECIEVE_MODE);
 }
 
 void loop() {
-  int data = random(0, 100);
-  mySerial.write(data);
-
-  Serial.print("Data sent: ");
-  Serial.println(data);
-
-  delay(2000);
-
+  String message = "Hello";
+  transmit(message);
+  Serial.print("[TX] Sent: ");
+  Serial.println();
+  delay(1000);
 }
