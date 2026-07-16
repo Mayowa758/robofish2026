b581acc13127530b4a959bc6631130bd1bff3faa
Mayowa Oyekan <z5592472@ad.unsw.edu.au>
2026-07-16 15:36:15 +1000

    wireless changes

--- reciever/src/main.cpp
+++ reciever/src/main.cpp
@@ -1,6 +1,12 @@
 #include <Arduino.h>
 #include <comms.h>
 #include <ESP32Servo.h>
+#include <ArduinoOTA.h>
+#include <WiFi.h>
+
+/* Wifi Connection */
+const char *ssid = "robofish";
+const char *password = "fih";
 
 /* Servo STUFF */
 
@@ -32,63 +38,74 @@ void calibrateServos();
 
 FishState state;
 
-
-int8_t smoothStep(int8_t current, int8_t target) {
-    if (current == target) return current;
+int8_t smoothStep(int8_t current, int8_t target)
+{
+    if (current == target)
+        return current;
 
     float weighted_sum = (1 - SENSITIVITY) * current + target * SENSITIVITY;
-    int8_t next = (int8_t) roundf(weighted_sum);
+    int8_t next = (int8_t)roundf(weighted_sum);
 
-    if (next == current) {
+    if (next == current)
+    {
         next = current + (target > current ? 1 : -1);
     }
 
     return next;
 }
 
-void tickState(FishState targetState) {
-    if (targetState.calibrate == CALIBRATE) {
+void tickState(FishState targetState)
+{
+    if (targetState.calibrate == CALIBRATE)
+    {
         calibrateServos();
         state.calibrate = NO_CHANGE;
     }
 
-    if (targetState.leftFin != NO_CHANGE) {
+    if (targetState.leftFin != NO_CHANGE)
+    {
         state.leftFin = smoothStep(state.leftFin, targetState.leftFin);
     }
 
-    if (targetState.rightFin != NO_CHANGE) {
+    if (targetState.rightFin != NO_CHANGE)
+    {
         state.rightFin = smoothStep(state.rightFin, targetState.rightFin);
     }
 
-    if (targetState.speed != NO_CHANGE) {
+    if (targetState.speed != NO_CHANGE)
+    {
         state.speed = smoothStep(state.speed, targetState.speed);
     }
 }
-void print_state(FishState state) {
-  Serial.println("--- Fish State ---");
-  
-  Serial.print("Left Fin:  ");
-  Serial.println(state.leftFin);
-  
-  Serial.print("Right Fin: ");
-  Serial.println(state.rightFin);
-  
-  Serial.print("Speed:     ");
-  Serial.println(state.speed);
-  
-  Serial.print("Calibrate: ");
-  Serial.println(state.calibrate);
-  
-  Serial.println("------------------");
+void print_state(FishState state)
+{
+    Serial.println("--- Fish State ---");
+
+    Serial.print("Left Fin:  ");
+    Serial.println(state.leftFin);
+
+    Serial.print("Right Fin: ");
+    Serial.println(state.rightFin);
+
+    Serial.print("Speed:     ");
+    Serial.println(state.speed);
+
+    Serial.print("Calibrate: ");
+    Serial.println(state.calibrate);
+
+    Serial.println("------------------");
 }
 
-void fishWave() {
+void fishWave()
+{
 
     int amp = map(state.speed, 0, 20, 10, 30);
     int speedDelay = map(state.speed, 0, 20, 20, 5);
-    if (state.speed <= 1) return; 
+    if (state.speed <= 1)
+        return;
 
-    if (state.speed > 1 && (millis() - lastWaveUpdate >= speedDelay )) {
+    if (state.speed > 1 && (millis() - lastWaveUpdate >= speedDelay))
+    {
 
         lastWaveUpdate = millis();
 
@@ -96,11 +113,13 @@ void fishWave() {
 
         wavePosition = constrain(wavePosition, -amp, amp);
 
-        if (wavePosition >= amp) {
+        if (wavePosition >= amp)
+        {
             waveDirection = -1;
         }
 
-        if (wavePosition <= -amp) {
+        if (wavePosition <= -amp)
+        {
             waveDirection = 1;
         }
 
@@ -113,12 +132,14 @@ void fishWave() {
     }
 }
 
-void updateSideFins() {
+void updateSideFins()
+{
     s2.write(constrain(s2_neutral + state.leftFin, 0, 180));
     s3.write(constrain(s3_neutral + state.rightFin, 0, 180));
 }
 
-void calibrateServos() {
+void calibrateServos()
+{
 
     s1_neutral = 90;
     s2_neutral = 90;
@@ -134,8 +155,8 @@ void calibrateServos() {
     state.rightFin = 0;
 }
 
-
-void setup() {
+void setup()
+{
     Serial.begin(115200);
 
     Serial1.begin(115200, SERIAL_8N1, RX1_PIN, TX1_PIN);
@@ -144,20 +165,33 @@ void setup() {
     s2.attach(S2_PIN);
     s3.attach(S3_PIN);
 
-    //Neutral positions
+    // Neutral positions
     s1.write(s1_neutral);
     s2.write(s2_neutral);
     s3.write(s3_neutral);
 
+    WiFi.begin(ssid, password);
+
+    // Enable WiFi Connection
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        delay(500);
+    }
+
+    ArduinoOTA.begin(); // Starts OTA
+
     delay(1000);
 }
 
-void loop() {
-    if (Serial1.available()) {
+void loop()
+{
+    ArduinoOTA.handle();
+    if (Serial1.available())
+    {
         String message = Serial1.readStringUntil('\n');
         message.trim();
-        
-        uint32_t int_message = (uint32_t) strtoul(message.c_str(), NULL, 10);
+
+        uint32_t int_message = (uint32_t)strtoul(message.c_str(), NULL, 10);
 
         FishState targetState = decode_state(int_message);
         tickState(targetState);
